368ac0b21eae723b8ee55ca6a931c81857ce66a4
EnTeeKiu <adaqetvse@gmail.com>
2026-05-26 14:44:46 +0700

    Convert website to Admin Portal for banhang store

--- JS/app.js
+++ JS/app.js
@@ -14,30 +14,14 @@ $(document).ready(function () {
         e.preventDefault();
         let username = $('.form-input').eq(0).val();
         let password = $('#password').val();
+        
         if (username === "" || password === "") {
             alert("Please input your username and password!");
         } else if (username === "admin" && password === "admin") {
             alert("Login success! Welcome Admin");
-            window.location.href = "admin.html";
-        } else {
-            alert("Login success! Hello " + username);
             window.location.href = "home.html";
-        }
-    });
-
-    $('#form-register').submit(function (e) {
-        e.preventDefault();
-        let username = $('#reg-username').val();
-        let password = $('#reg-password').val();
-        let confirm = $('#reg-confirm-password').val();
-        
-        if (username === "" || password === "" || confirm === "") {
-            alert("Please input all information!");
-        } else if (password !== confirm) {
-            alert("Passwords do not match!");
         } else {
-            alert("Register success! Welcome " + username);
-            window.location.href = "home.html";
+            alert("Invalid credentials! Only admins can access this portal.");
         }
     });
 });
